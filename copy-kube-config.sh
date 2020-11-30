591a4ad8c8a7543fd41a69ff4d80192fc2b1c3c1
root <root@master.asia-southeast1-b.c.striped-reserve-266903.internal>
2020-11-30 19:26:29 +0000

    etcdctl

--- copy-kube-config.sh
+++ copy-kube-config.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash 
 mkdir -p ~/.kube/
-scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $1:~/.kube/config  ~/.kube/config
+scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $1:~/.kube/config  ~/.kube/config-$master
 chown $(id -u):$(id -g) ~/.kube/config
 sed -i '/source <(kubectl completion bash)/d'  ~/.bashrc
 echo 'source <(kubectl completion bash)' >>~/.bashrc
--- utils.sh
+++ utils.sh
@@ -2,6 +2,9 @@
 export usr=$(whoami)
 read_setup()
 {
+  
+  master=
+  workers=
   while IFS="=" read -r key value; do
     case "$key" in
       "master") export master="$value" ;;
@@ -29,5 +32,23 @@ sleep_few_secs()
 #Launch busybox container called debug
 k8_debug()
 {
+ print_msg "Setting up busybox debug container"
  kubectl run -i --tty --rm debug --image=busybox:1.28 --restart=Never -- sh 
 }
+
+install_etcdctl()
+{
+ ETCD_VER="3.4.14"
+ ETCD_VER=${1:-$ETCD_VER}
+ DOWNLOAD_URL=https://github.com/etcd-io/etcd/releases/download
+ print_msg "Downloading etcd $ETCD_VER from $DOWNLOAD_URL"
+ wget -q --timestamping ${DOWNLOAD_URL}/v${ETCD_VER}/etcd-v${ETCD_VER}-linux-amd64.tar.gz -O /tmp/etcd-v${ETCD_VER}-linux-amd64.tar.gz
+ rm -rf /tmp/etcd-download-loc
+ mkdir /tmp/etcd-download-loc
+ tar xzf /tmp/etcd-v${ETCD_VER}-linux-amd64.tar.gz -C /tmp/etcd-download-loc --strip-components=1
+ mv /tmp/etcd-download-loc/etcdctl /usr/local/bin
+ which etcdctl
+ etcdctl version
+
+}
+
