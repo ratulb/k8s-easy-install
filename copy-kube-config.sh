013154d0f152d74ef1563ce8ebf3d419714f3c85
ratulb <ratul75@hotmail.com>
2020-12-09 06:55:17 +0000

    Kube config again gets added to .kube

--- copy-kube-config.sh
+++ copy-kube-config.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash 
 mkdir -p ~/.kube/
+#scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $1:~/.kube/config  ~/.kube/config-$master
 scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $1:~/.kube/config  ~/.kube/config-$master
 chown $(id -u):$(id -g) ~/.kube/config
 sed -i '/source <(kubectl completion bash)/d'  ~/.bashrc
 echo 'source <(kubectl completion bash)' >>~/.bashrc
+source ~/.bashrc
 
