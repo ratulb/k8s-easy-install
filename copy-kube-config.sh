be920fe8d23481a553b451f8f6eed7e99c82fd0f
ratulb <ratul75@hotmail.com>
2020-12-09 07:49:03 +0000

    kube config to workstation

--- copy-kube-config.sh
+++ copy-kube-config.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash 
 mkdir -p ~/.kube/
 #scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $1:~/.kube/config  ~/.kube/config-$master
-scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $1:~/.kube/config  ~/.kube/config-$master
+scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $1:~/.kube/config  ~/.kube/config
 chown $(id -u):$(id -g) ~/.kube/config
 sed -i '/source <(kubectl completion bash)/d'  ~/.bashrc
 echo 'source <(kubectl completion bash)' >>~/.bashrc
