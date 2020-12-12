7f46f72618615e19b1921ab6a9acf843b9720a1d
Ratul Buragohain <ratulb@hotmail.com>
2020-12-12 07:44:44 +0000

    install kubectl when managing node is not part of the cluster

---
+++ init-self.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash 
+#If this host is not part of the cluster 
+. utils.sh
+this_host_ip=$(hostname -i)
+
+if [ "$master" == *"$this_host_ip"*  -o "$etcd_servers" == *"$this_host_ip"* ]; then
+  exit 0
+fi
+
+curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+mkdir -p ~/.kube/
+#scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $1:~/.kube/config  ~/.kube/config-$master
+sudo -u $usr scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $master:~/.kube/config  ~/.kube/
+chown $(id -u):$(id -g) ~/.kube/config
+sed -i '/source <(kubectl completion bash)/d'  ~/.bashrc
+echo 'source <(kubectl completion bash)' >>~/.bashrc
+source ~/.bashrc
+
+kubectl get pod
+
