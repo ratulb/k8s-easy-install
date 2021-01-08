b2aecce91bba8419bbe42148bd5593c4135c0f0a
root <root@box-1.asia-southeast1-b.c.striped-reserve-266903.internal>
2021-01-08 15:42:42 +0000

    change for ubuntu-20.04

--- install-docker.sh
+++ install-docker.sh
@@ -7,31 +7,18 @@ sudo apt update && sudo apt install -y \
   apt-transport-https ca-certificates curl software-properties-common gnupg2
 
 # Add Docker's official GPG key:
-if [ -f /etc/debian_version ]; then
-  curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-  sudo apt update
-  sudo apt install docker-ce
-  sudo usermod -aG docker ${USER}
-
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo \
+  apt-key --keyring /etc/apt/trusted.gpg.d/docker.gpg add -
+# Add the Docker apt repository:
+sudo cat /etc/*release | grep VERSION_ID=\"20.04\"
+if [ "$?" -eq 0 ]; then
+  sudo add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu eoan stable"
 else
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo \
-    apt-key --keyring /etc/apt/trusted.gpg.d/docker.gpg add -
-  # Add the Docker apt repository:
-  sudo cat /etc/*release | grep VERSION_ID=\"20.04\"
-  if [ "$?" -eq 0 ]; then
-    sudo add-apt-repository \
-      "deb [arch=amd64] https://download.docker.com/linux/ubuntu eoan stable"
-  else
-    sudo add-apt-repository \
-      "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-  fi
-  # Install Docker CE
-  sudo apt update && sudo apt install -y \
-    containerd.io=1.2.13-2 \
-    docker-ce=5:19.03.11~3-0~ubuntu-$(lsb_release -cs) \
-    docker-ce-cli=5:19.03.11~3-0~ubuntu-$(lsb_release -cs)
+  sudo add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 fi
+sudo apt update && sudo apt install -y docker-ce docker-ce-cli containerd.io
 # Set up the Docker daemon
 cat <<EOF | sudo tee /etc/docker/daemon.json
 {
