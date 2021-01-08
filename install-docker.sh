d449df53195c21b44a278a3a503e447484c9a86c
ratulb <ratul75@hotmail.com>
2021-01-08 19:07:01 +0000

    Clean before install

--- install-docker.sh
+++ install-docker.sh
@@ -6,6 +6,7 @@ sudo apt update
 sudo apt update && sudo apt install -y \
   apt-transport-https ca-certificates curl software-properties-common gnupg2
 
+sudo apt-get remove -y docker docker-engine docker.io containerd runc
 home_url=$(sudo cat /etc/*release | grep HOME_URL | cut -d'"' -f2)
 if [ "$home_url" = "https://www.ubuntu.com/" ]; then
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
