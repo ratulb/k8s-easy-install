310b8a2b0c68e6456aa3c995e0b140dfaf4df0e8
Ratul Buragohain <ratul75@hotmail.com>
2021-01-30 08:38:32 +0000

    .kube/config for masters

--- init-self.sh
+++ init-self.sh
@@ -10,14 +10,15 @@ if ! [[ "$cluster_members" = *"$this_host_name"* ]] && ! [[ "$cluster_members" =
   sudo mv ./kubectl /usr/local/bin/kubectl
 fi
 
-if [[ "$masters" = *"$this_host_name"* ]] || [[ "$masters" = *"$this_host_ip"* ]]; then
-  :
+if [[ "$masters" =~ "$this_host_name" ]] || [[ "$masters" =~ "$this_host_ip" ]]; then
+  sudo cp /etc/kubernetes/admin.conf ~/.kube/config
 else
   master_1=$(echo $masters | cut -d' ' -f1)
   mkdir -p $HOME/.kube/
   remote_copy $master_1:$HOME/.kube/config $HOME/.kube/config
-  chown $(id -u):$(id -g) $HOME/.kube/config
-  sed -i '/source <(kubectl completion bash)/d' $HOME/.bashrc
-  echo 'source <(kubectl completion bash)' >>$HOME/.bashrc
-  source $HOME/.bashrc
 fi
+
+chown $(id -u):$(id -g) ~/.kube/config
+sed -i '/source <(kubectl completion bash)/d' ~/.bashrc
+echo 'source <(kubectl completion bash)' >>~/.bashrc
+source ~/.bashrc
